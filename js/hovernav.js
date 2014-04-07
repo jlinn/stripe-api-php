20760148f61852173e2a4b1848a2bd8ef6aa57a7
jlinn <jlinn5732@gmail.com>
2014-04-06 19:09:44 -0700

    Update gh-pages to use generated docs

---
+++ js/hovernav.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    /*
+     "Hovernav" navbar dropdown on hover
+     Delete this segment if you don't want it, and delete the corresponding CSS in bst.css
+     Uses jQuery Media Query - see http://www.sitepoint.com/javascript-media-queries/
+     */
+    var mq = window.matchMedia('(min-width: 768px)');
+    if (mq.matches) {
+        $('ul.navbar-nav li').addClass('hovernav');
+    } else {
+        $('ul.navbar-nav li').removeClass('hovernav');
+    };
+    /*
+     The addClass/removeClass also needs to be triggered
+     on page resize <=> 768px
+     */
+    if (matchMedia) {
+        var mq = window.matchMedia('(min-width: 768px)');
+        mq.addListener(WidthChange);
+        WidthChange(mq);
+    }
+    function WidthChange(mq) {
+        if (mq.matches) {
+            $('ul.navbar-nav li').addClass('hovernav');
+        } else {
+            $('ul.navbar-nav li').removeClass('hovernav');
+        }
+    };
+
+});
